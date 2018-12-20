6e21b9e789f52226590356ab9a5df903531ad0cb
littletry <1109394634@qq.com>
2018-12-20 16:04:05 +0800

    因方法watch的位置写错，从而核对了好几遍代码，都没看出来，真TM尴尬

--- test_07/tabs/pane.js
+++ test_07/tabs/pane.js
@@ -4,10 +4,32 @@ Vue.component('pane', {
         <div class="pane" v-show="show">\
             <slot></slot>\
         </div>',
+    props: {
+        name: {
+            type: String
+        },
+        label: {
+            type: String,
+            default: ''
+        }
+    },
     data: function () {
         /*pane需要控制标签页内容的显示与隐藏，设置一个data: show，井且用v-show指令来控制元素*/
         return {
             show: true
         }
+    },
+    methods: {
+        updateNav() {
+            this.$parent.updateNav();
+        }
+    },
+    watch: {
+        lable() {
+            this.updateNav();
+        }
+    },
+    mounted() {
+        this.updateNav();
     }
 });
