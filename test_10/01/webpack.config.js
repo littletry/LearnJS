16f25f4387f429b64dede9e88f3412d8c2c76437
littletry <1109394634@qq.com>
2018-12-24 10:43:30 +0800

    webpack

---
+++ test_10/01/webpack.config.js
@@ -0,0 +1,35 @@
+import ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let path = require('path');
+
+let config = {
+    entry: {
+        main: './main'
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                /*use: [
+                    'style-loader',
+                    'css-loader'
+                ]*/
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                    fallback: 'style-loader'
+                })
+            }
+        ]
+    },
+    plugins: [
+        //重命名提取后的css文件
+        new ExtractTextPlugin("main.css")
+    ]
+};
+
+module.exports = config;
