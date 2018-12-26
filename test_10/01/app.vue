b0bef725ea3b63b7f5378568b115f4f0c1b65ace
littletry <1109394634@qq.com>
2018-12-26 17:26:59 +0800

    根实例app.vue导入title.vue和button.vue

--- test_10/01/app.vue
+++ test_10/01/app.vue
@@ -1,9 +1,26 @@
 <template>
-    <div>Hello {{ name }}</div>
+    <div>
+        <v-title title="Vue组件化"></v-title>
+        <v-button @click="handleClick">点击按钮</v-button>
+    </div>
 </template>
 
 <script>
+    //导入组件
+    import vTitle from './title.vue';
+    import vButton from './button.vue';
+
     export default {
+        //对象字面量缩写。当对象的key和value名称一致时，可以缩写成一个。
+        components: {
+            vTitle,
+            vButton
+        },
+        methods: {
+            handleClick(e) {
+                console.log(e);
+            }
+        },
         data() {
             return {
                 name: 'Vue.js'
