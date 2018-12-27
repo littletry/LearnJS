ada14756b0960394f8070b98c3014c7dd4a98ecb
littletry <1109394634@qq.com>
2018-12-27 11:26:14 +0800

    动态更改title，跳转后滚动到顶部

--- test_11_插件/main.js
+++ test_11_插件/main.js
@@ -11,10 +11,16 @@ Vue.use(VueRouter);
 const Routers = [
     {
         path: '/index',
+        meta: {
+            title: '首页'
+        },
         component: (resolve) => require(['./views/index.vue'], resolve)
     },
     {
         path: '/about',
+        meta: {
+            title: '关于'
+        },
         component: (resolve) => require(['./views/about.vue'], resolve)
     },
     {
@@ -36,6 +42,17 @@ const RouterConfig = {
     routes: Routers
 };
 const router = new VueRouter(RouterConfig);
+//修改title，在页面发生路由改变时，统一设置。
+// vue-router提供了导航钩子beforeEach和afterEach，它们会在路由即将改变前和改变后触发，
+// 所以设置标题可以在beforeEach钩子完成。
+router.beforeEach((to, from, next) => {
+    window.document.title = to.meta.title;
+    next();
+});
+//一个页面较长，滚动到某个位置，再跳转到另一个页面，滚动条默认是在上一个页面停留的位置，而好的体验肯定是能返回顶端。
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0);
+});
 
 document.getElementById('app').innerHTML = 'Hello webpack';
 
