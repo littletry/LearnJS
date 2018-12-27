e4599ccf77cfc6255c3aadbb95d98587ae794428
littletry <1109394634@qq.com>
2018-12-27 11:05:17 +0800

    实现简单的路由

--- test_11_插件/main.js
+++ test_11_插件/main.js
@@ -1,13 +1,40 @@
 import './style.css';
 //导入vue框架
 import Vue from 'vue';
+
+import VueRouter from 'vue-router';
 //导入app.vue组件
 import App from './app.vue';
+
+Vue.use(VueRouter);
+
+const Routers = [
+    {
+        path: '/index',
+        component: (resolve) => require(['./views/index.vue'], resolve)
+    },
+    {
+        path: '/about',
+        component: (resolve) => require(['./views/about.vue'], resolve)
+    },
+    {
+        path: '*',
+        redirect: '/index'
+    }
+];
+
+const RouterConfig = {
+    // 使用HTML5的History路由模式
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
 
 document.getElementById('app').innerHTML = 'Hello webpack';
 
 //创建Vue根实例
 new Vue({
     el: '#app',
+    router: router,
     render: h => h(App)
 });
