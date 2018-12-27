a62a3a5466ddcbe0f344365d816ccf7146ca147c
littletry <1109394634@qq.com>
2018-12-27 11:17:13 +0800

    vue-router的两种跳转页面的方法

--- test_11_插件/views/about.vue
+++ test_11_插件/views/about.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>介绍页</div>
+    <div>
+        <h1>介绍页</h1>
+        <button @click="handleRouter">跳转到user</button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "about"
+        name: "about",
+        methods: {
+            handleRouter() {
+                this.$router.push('/user/123456');
+            }
+        }
     }
 </script>
 
--- test_11_插件/views/index.vue
+++ test_11_插件/views/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>首页</div>
+    <div>
+        <h1>首页</h1>
+        <router-link to="/about">跳转到 about</router-link>
+    </div>
 </template>
 
 <script>
