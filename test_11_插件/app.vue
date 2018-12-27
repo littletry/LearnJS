153c3b3cab10971bdbe31651ea12f5b1b7830182
littletry <1109394634@qq.com>
2018-12-27 10:06:28 +0800

    第11章初始化，基于第10章

---
+++ test_11_插件/app.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-title title="Vue组件化"></v-title>
+        <v-button @click="handleClick">点击按钮</v-button>
+        <p>
+            <img src="./images/image.png" style="width: 200px">
+        </p>
+    </div>
+</template>
+
+<script>
+    //导入组件
+    import vTitle from './title.vue';
+    import vButton from './button.vue';
+
+    export default {
+        //对象字面量缩写。当对象的key和value名称一致时，可以缩写成一个。
+        components: {
+            vTitle,
+            vButton
+        },
+        methods: {
+            handleClick(e) {
+                console.log(e);
+            }
+        },
+        data() {
+            return {
+                name: 'Vue.js'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        color: #f60;
+        font-size: 24px;
+    }
+</style>
